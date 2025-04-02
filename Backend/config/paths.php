318ed12c424282e4bfa87790f0beee6aed8476c6
jasminrainer <wi23b034@technikum-wien.at>
2025-04-02 18:51:18 +0200

    Fixed Structure, Footer and Navbar

--- Backend/config/paths.php
+++ Backend/config/paths.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 // Pfade für include/require
 define('BASE_PATH', dirname(__DIR__, 1));
 define('DESIGN_PATH', BASE_PATH . '/design');
@@ -10,11 +9,3 @@ define('SITES_PATH', FRONTEND_PATH . '/sites');
 define('BASE_URL', '/Blattwerk_Aktualisiert_FooterFix/Frontend');
 define('SITES_URL', BASE_URL . '/sites');
 ?>
-=======
-// BASE_PATH = .../Backend
-define('BASE_PATH', dirname(__DIR__, 1));
-
-// DESIGN_PATH = .../Backend/design
-define('DESIGN_PATH', BASE_PATH . '/design');
-?>
->>>>>>> da82d357efba386bb3b06b2b5cd77b0aba84f850
--- Frontend/index.php
+++ Frontend/index.php
@@ -2,16 +2,12 @@
 require_once __DIR__ . '/../Backend/config/paths.php';
 include DESIGN_PATH . '/navbar.php';
 ?>
-<<<<<<< HEAD
-=======
 
->>>>>>> da82d357efba386bb3b06b2b5cd77b0aba84f850
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Blattwerk – Dein Pflanzen-Webshop</title>
     <link rel="stylesheet" href="/../css/style.css">
@@ -28,21 +24,3 @@ include DESIGN_PATH . '/navbar.php';
 </main>
 
 <?php include DESIGN_PATH . '/footer.php'; ?>
-=======
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Blattwerk – Dein Pflanzen-Webshop</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script defer src="js/test.js"></script>
-</head>
-<body>
-    
-    <main>
-        <section class="hero">
-            <h2>Willkommen bei Blattwerk</h2>
-            <p>Finde deine perfekte Zimmerpflanze – nachhaltig, schön und direkt zu dir geliefert.</p>
-            <a href="sites/products.php" class="btn">Jetzt entdecken</a>
-        </section>
-        </main>
-
-        <?php include DESIGN_PATH . '/footer.php'; ?>
->>>>>>> da82d357efba386bb3b06b2b5cd77b0aba84f850
